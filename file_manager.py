98a7594b6d986900cde2a62eaa547c1e8b39b3d6
tedros g/her <tedroswalker21@gmail.com>
2024-05-15 08:41:10 -0700

    adding two tests called test_change_to_valid_directory and test_change_to_root_directory for the change_directory() method

---
+++ file_manager.py
@@ -0,0 +1,21 @@
+import os
+import shutil
+import hashlib
+
+class FileManager:
+    def __init__(self):
+        self.current_directory = os.getcwd()
+
+    def change_directory(self, path):
+        """
+        Change the current working directory to the specified path.
+
+        Args:
+        path (str): The path to change the directory to.
+        """
+        if not path:
+            raise OSError("Path cannot be empty")
+        elif os.path.isdir(path):
+            self.current_directory = path
+        else:
+            raise OSError(f"{path} is not a valid directory.")
--- test_file_manager.py
+++ test_file_manager.py
@@ -1,18 +1,40 @@
-import os
 import unittest
-from file_manager import FileManager
+from unittest.mock import patch, mock_open
+import os
 import shutil
+import hashlib
+import tempfile
 
-class TestFileManager(unittest.TestCase):
-  '''this will contain all the test for the project File Manager'''
-  def setUp(self):
-    '''setting up the environment before executing each test method'''
-    self.file_manager = FileManager()
-    self.test_file_path = 'test_file.txt'
-    os.makedirs(self.test_dir, exist_ok=True)
-    print(f"Starting test: {self._testMethodName}")
+from file_manager import FileManager
+
+class TestFileManager(unittest.TestCase):	
+    def setUp(self):
+        self.file_manager = FileManager()
+        self.test_dir = os.path.join(os.path.dirname(__file__), 'test_files')
+        os.makedirs(self.test_dir, exist_ok=True)
+        self.test_file_path = os.path.join(self.test_dir, 'test.txt')
+        with open(self.test_file_path, 'w') as f:
+            f.write('test content')
+        os.chmod(self.test_file_path, 0o400)  # Set file to read-only
+        print(f"Starting test: {self._testMethodName}")
+
+    def tearDown(self):
+        os.chmod(self.test_file_path, 0o644)  # Restore file permissions
+        shutil.rmtree(self.test_dir)
+        print(f"Finished test: {self._testMethodName}")
+    
+        
+    def test_change_to_valid_directory(self):
+        ''' testing by passing valid directory'''
+        self.file_manager.change_directory(self.test_dir)
+        self.assertEqual(self.file_manager.current_directory, self.test_dir)
+
+    def test_change_to_root_directory(self):
+        ''' testing by passing the root of the curent directory'''
+        self.file_manager.change_directory('/')
+        self.assertEqual(self.file_manager.current_directory, '/')
+
   
-  def tearDown(self):
-    '''cleaning up the environment after executing each test method'''
-    shutil.rmtree(self.test_dir)
-    print(f"Finished test: {self._testMethodName}")
+
+if __name__ == '__main__':
+    unittest.main()
