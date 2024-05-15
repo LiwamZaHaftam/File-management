15b7e66ff965d43e3f283fc51ea5c6650b13e272
Zehara Kasaw <zahrakasaw322@gmail.com>
2024-05-15 09:19:31 -0700

    added the method change directory, since all the needed tests are added

--- file_manager.py
+++ file_manager.py
@@ -5,14 +5,8 @@ import hashlib
 class FileManager:
     def __init__(self):
         self.current_directory = os.getcwd()
-
     def change_directory(self, path):
-        """
-        Change the current working directory to the specified path.
 
-        Args:
-        path (str): The path to change the directory to.
-        """
         if not path:
             raise OSError("Path cannot be empty")
         elif os.path.isdir(path):
